3bfc8c7488566ea871fe8fcbb573a338b1f7edfb
Mohamed Esmael <mohamed.2714104@gmail.com>
2025-11-24 18:37:44 +0200

    Add files via upload

--- Terraform/terraform-infra/backend.tf
+++ Terraform/terraform-infra/backend.tf
@@ -1,15 +1,21 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "7.1.1"
-    }
-  }
+# terraform {
+#   required_providers {
+#     google = {
+#       source  = "hashicorp/google"
+#       version = "7.1.1"
+#     }
+#   }
 
-  backend "gcs" {
-    bucket      = "terrraform-rag-app-state"
-    prefix      = "dev"
-    credentials = "/home/esmael/Downloads/rock-task-468906-n1-7263e54c7ecd.json"  
-  }
+#   backend "gcs" {
+#     bucket      = "terrraform-rag-app-state"
+#     prefix      = "dev"
+#     credentials = "/home/esmael/Downloads/rock-task-468906-n1-7263e54c7ecd.json"  
+#   }
   
+# }
+
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
 }
